b51e47a27e099863fde88cfbb998f6afab1df538
SigCiccio <mailpercoseacaso9909@gmail.com>
2023-10-31 14:25:57 +0100

    end prototype

---
+++ main.js
@@ -0,0 +1,22 @@
+/* AIzaSyB7SCPWioTnCJikbu-4KihUHNygkADIQ80 */
+
+
+
+function initMap() {
+  const myLatLng = {
+    lat: 44.50951401998059,
+    lng: 11.300555926121575
+  };
+  const map = new google.maps.Map(document.getElementById("gmp-map"), {
+    zoom: 14,
+    center: myLatLng,
+    fullscreenControl: false,
+    zoomControl: true,
+    streetViewControl: false
+  });
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Sono qui"
+  });
+}
